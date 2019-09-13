2545a4e06c636ffe001c05e49e95f057f39fccc2
Anastasiia Lisikh <lisikh.anastasiia@gmail.com>
2019-09-13 11:23:28 +0200

    recipe book, chapter 3, end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentTab = 'recipes';
+
+  onSelectCurrentTab(currentTab: string) {
+    this.currentTab = currentTab;
+  }
 }
